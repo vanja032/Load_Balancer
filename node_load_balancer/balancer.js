e64ad1b0c77225ada3115e6b68bf613c454384d3
vanja032 <sretenovic.vanja32@gmail.com>
2023-04-15 01:29:16 +0200

    Added NodeJs clusters to the Load Balancer.

--- node_load_balancer/balancer.js
+++ node_load_balancer/balancer.js
@@ -21,16 +21,14 @@ for(const url in urls){
         onProxyReq: (proxy_request, request, response) => {
             const target_url = `${proxy_request.protocol}//${proxy_request.host}`;
             //console.log(`Incoming request for: ${target_url}`);
-            proxyPreRequestMiddleware(request, response, () => {});
-            console.log(urls);
             urls[target_url] += 1;
+            //console.log(urls);
             response.setHeader("X-Target-URL", `${target_url}`);
         },
         onProxyRes: (proxy_response, request, response) => {
             const target_url = `${response.getHeader("X-Target-URL")}`;
             //console.log(`Incoming result from: ${target_url}`);
             if(urls[target_url] > 0) urls[target_url] -= 1;
-            //proxyPreResultMiddleware(request, response, () => {}); // Call the pre-result proxy middleware to update the target for the next request
         }
     });
 }
@@ -45,24 +43,6 @@ const getNextTarget = () => {
     return null; // Return null if all targets are saturated
 };
 
-
-// Create a pre-request middleware function that blocks requests if all targets are saturated
-const proxyPreRequestMiddleware = (request, response, next) => {
-    const target = getNextTarget(); // Call the custom target function
-    if (target == null) {
-        return response.status(503).send("All target URLs are saturated."); // Return 503 status if all targets are saturated
-    }
-    next();
-};
-
-// Create a pre-result middleware function that modifies the 'target' option based on the incoming request
-const proxyPreResultMiddleware = (request, response, next) => {
-    const target = getNextTarget(); // Call the custom target function
-    if (target != null) {
-        proxy.target = target; // Update proxy middleware with updated target
-    }
-    next();
-};
 
 const proxySelector = () => {
     const nextTarget = getNextTarget();
--- node_load_balancer/server.js
+++ node_load_balancer/server.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
-
-const balancer = require("./balancer");
+const cluster = require("cluster");
+const available_cores = require("os").cpus().length;
 
 const balancer_host = process.env.BALANCER_HOST || "0.0.0.0";
 const balancer_port = process.env.BALANCER_PORT || 3000;
 
-balancer.listen(balancer_port, balancer_host, () => {
-    console.log("NodeJs Balancer server has started.");
-});
+const balancer = require("./balancer");
+
+if(cluster.isMaster){
+    for(let i = 0; i < available_cores; i++){
+        cluster.fork();
+    }
+}
+else{
+    balancer.listen(balancer_port, balancer_host, () => {
+        console.log("NodeJs Balancer server has started.");
+    });
+}
